419ce74cd9a4d5246b716e3b024f959fd671b976
Silas John <sejohn@usc.edu>
2024-12-28 13:47:08 -0700

    semi format 3 top champs

--- format.py
+++ format.py
@@ -29,5 +29,16 @@ for header in new_headers:
 # in new df, every instance of '\n' replace with '\t'
 new_df = new_df.replace('\n', '\t', regex=True)
 
+# for the header "top_3_champs", split the string into a list of strings by ',' or ' ' or "/"
+new_df['top_3_champs'] = new_df['top_3_champs'].apply(lambda x: x.split(',') if ',' in x else x.split(' ') if ' ' in x else x.split('/'))
+
+# get rid of whitespace in the beginning and end of each string in the list
+new_df['top_3_champs'] = new_df['top_3_champs'].apply(lambda x: [champ.strip() for champ in x])
+
+# print list of unique champions from 'top_3_champs', sort alphabetically
+champions = set()
+new_df['top_3_champs'].apply(lambda x: [champions.add(champ) for champ in x])
+print(f'Champions: {sorted(champions)}')
+
 # export df into new csv file in data folders
 new_df.to_csv('data/formatted_data.csv', index=False)
